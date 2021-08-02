ac2ec9cdb39a44d7dfda5a6888c671aaf422cf1a
Amruth Kuntamalla <amruthk99@gmail.com>
2021-08-03 01:50:13 +0530

    added AOS

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import 'tailwindcss/tailwind.css'
+import { useEffect } from "react";
+import AOS from "aos";
+import "tailwindcss/tailwind.css";
+import "aos/dist/aos.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    AOS.init({
+      once: true, // whether animation should happen only once - while scrolling down
+    });
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
